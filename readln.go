89fd0672afc9f0aba94814e0bcbdbe13eb0244ba
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-08-05 20:33:59 -0700

    initial commits

--- readln.go
+++ readln.go
@@ -7,6 +7,14 @@ import (
 // Readln reads in a line from os.Stdin and puts it at ‘dst’.
 //
 // rcv.Readln() does what you probably thought fmt.Scanln() should do (but doesn't).
+//
+// Here is an example:
+//
+//	var line string
+//	
+//	rcv.Readlne(&dst)
+//	
+//	fmt.Printf("LINE: %q\n", line)
 func Readln(dst interface{}) (n int, err error) {
 	return Freadln(os.Stdin, dst)
 }
