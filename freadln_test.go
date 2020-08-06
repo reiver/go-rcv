974b5551019b18cd0e8b29f88d65389ff2b194a9
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-08-05 21:34:58 -0700

    initial commits

---
+++ freadln_test.go
@@ -0,0 +1,89 @@
+package rcv_test
+
+import (
+	"github.com/reiver/go-rcv"
+
+	"io"
+	"strings"
+
+	"testing"
+)
+
+func TestFreadln(t *testing.T) {
+	tests := []struct{
+		Input     string
+		Expected  string
+		ExpectedN int
+	}{
+		{
+			//                                     line feed
+			Input:          "apple banana cherry"+"\u000A"+"one two three",
+			Expected:       "apple banana cherry",
+			ExpectedN : len("apple banana cherry"+"\u000A"),
+		},
+		{
+			//                                     vertical tab
+			Input:          "apple banana cherry"+"\u000B"+"one two three",
+			Expected:       "apple banana cherry",
+			ExpectedN : len("apple banana cherry"+"\u000B"),
+		},
+		{
+			//                                     form feed
+			Input:          "apple banana cherry"+"\u000C"+"one two three",
+			Expected:       "apple banana cherry",
+			ExpectedN : len("apple banana cherry"+"\u000C"),
+		},
+		{
+			//                                     carriage return
+			Input:          "apple banana cherry"+"\u000D"+"one two three",
+			Expected:       "apple banana cherry",
+			ExpectedN : len("apple banana cherry"+"\u000D"),
+		},
+		{
+			//                                     next line
+			Input:          "apple banana cherry"+"\u0085"+"one two three",
+			Expected:       "apple banana cherry",
+			ExpectedN : len("apple banana cherry"+"\u0085"),
+		},
+		{
+			//                                     line separator
+			Input:          "apple banana cherry"+"\u2028"+"one two three",
+			Expected:       "apple banana cherry",
+			ExpectedN : len("apple banana cherry"+"\u2028"),
+		},
+		{
+			//                                     paragraph separator
+			Input:          "apple banana cherry"+"\u2029"+"one two three",
+			Expected:       "apple banana cherry",
+			ExpectedN : len("apple banana cherry"+"\u2029"),
+		},
+	}
+
+	for testNumber, test := range tests {
+
+		var reader io.Reader = strings.NewReader(test.Input)
+
+		var dst string
+		n, err := rcv.Freadln(reader, &dst)
+
+		if nil != err {
+			t.Errorf("For test #%d, received an error, but did not actually expect one.", testNumber)
+			t.Logf("ERROR: (%T) %q", err, err)
+			continue
+		}
+		if expected, actual := test.ExpectedN, n; expected != actual {
+			t.Errorf("For test #%d, the actual number of bytes read is not what was expected.", testNumber)
+			t.Logf("INPUT:     %q", test.Input)
+			t.Logf("RESD LINE: %q", dst)
+			t.Logf("EXPECTED: %d", expected)
+			t.Logf("ACTUAL:   %d", actual)
+			continue
+		}
+		if expected, actual := test.Expected, dst; expected != actual {
+			t.Errorf("For test #%d, the read line is not what was expected.", testNumber)
+			t.Logf("EXPECTED: %q", expected)
+			t.Logf("ACTUAL:   %q", actual)
+			continue
+		}
+	}
+}
